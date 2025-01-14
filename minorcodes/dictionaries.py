ae51849afb173fee2f84a182e9f6470ebc55fdbc
SpxceMan <anshul.sekuri@gmail.com>
2025-01-14 19:30:43 +0530

    Dictionary Basics

---
+++ minorcodes/dictionaries.py
@@ -0,0 +1,8 @@
+capitals = {"USA": "Washington DC",
+            "India":"New Delhi",
+            "Japan":"Tokyo",
+            "Italy":"Rome"}
+
+
+for key,value in capitals.items():
+    print(f"{key}: {value}")
